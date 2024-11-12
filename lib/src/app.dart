aa5535cdb4bb928713b16c0db890048db44f672b
BZR4 <bzr4@icloud.com>
2024-11-11 21:09:41 -0300

    EcoPonto

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,6 @@
+import 'package:apps_playground/src/core/constants.dart';
+import 'package:apps_playground/src/features/ecoponto/presentation/ajude_o_planeta_page.dart';
+import 'package:apps_playground/src/features/ecoponto/presentation/saldo_page.dart';
 import 'package:apps_playground/src/features/impopar/presentation/impopar.dart';
 import 'package:apps_playground/src/features/insanelybutton/presentation/insanely_button_page.dart';
 import 'package:flutter/material.dart';
@@ -9,11 +12,18 @@ import 'features/sounds/presentation/sounds_page.dart';
 class App extends StatelessWidget {
   const App({super.key});
 
+  static final url =
+      'https://images.pexels.com/photos/8542564/pexels-photo-8542564.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        '/': (context) => const HomePage(),
+        '/': (context) => SaldoPage(
+            // titulo: 'Ajude o Planeta',
+            // conteudo: kMussumIpsum,
+            // urlImagem: url,
+            ),
         '/sounds': (_) => const SoundsPage(),
         '/insanebutton': (_) => const InsanelyButtonPage(),
         '/impopar': (_) => const ImpoParPage(),
---
+++ lib/src/core/constants.dart
@@ -0,0 +1,2 @@
+const kMussumIpsum =
+    'Mussum Ipsum, cacilds vidis litro abertis. Quem manda na minha terra sou euzis! Casamentiss faiz malandris se pirulitá.';
